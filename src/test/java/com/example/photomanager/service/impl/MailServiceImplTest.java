60c8d9a48c1d680b4b1f148a8f729c1395086b85
SupremeXiaoZhao <2410252220@qq.com>
2020-04-30 14:22:06 +0800

    测试邮箱功能

---
+++ src/test/java/com/example/photomanager/service/impl/MailServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.example.photomanager.service.impl;
+
+import com.example.photomanager.service.QZ_MailService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author 小朝
+ * @date 2020/4/30
+ **/
+@SpringBootTest
+public class MailServiceImplTest {
+    @Autowired
+    private QZ_MailService qz_mailService;
+
+    @Test
+    public void test(){
+        qz_mailService.sendSimpleMail("541099558@qq.com","验证码","123456");
+    }
+}
