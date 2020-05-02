ac9757f1c2834f1ae29102bea1fb726ec8270d64
SupremeXiaoZhao <2410252220@qq.com>
2020-05-02 10:58:53 +0800

    添加了登录，发送验证码，比对验证码，修改密码

--- src/main/java/com/example/photomanager/service/UserService.java
+++ src/main/java/com/example/photomanager/service/UserService.java
@@ -6,6 +6,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.photomanager.bean.vo.UserInfo;
 import com.example.photomanager.enums.UserInfoEnum;
 
+import javax.servlet.http.HttpSession;
+
 public interface UserService extends IService<User> {
     /**
      * 修改用户信息
@@ -17,4 +19,12 @@ public interface UserService extends IService<User> {
     boolean checkUsername(String username);
 
     boolean checkEmail(String email);
+
+    boolean login(String username, String password, Boolean rememberMe);
+
+    boolean getActiveCode(String email, HttpSession session);
+
+    boolean checkActiveCode(String email, String activeCode, HttpSession session);
+
+    boolean modifyPassword(String email, String password);
 }
