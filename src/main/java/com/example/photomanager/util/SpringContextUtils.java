5ff56987b495c847ea370bca7191e64a5a348860
SupremeXiaoZhao <2410252220@qq.com>
2020-05-02 10:56:49 +0800

    静态获取bean的工具类

---
+++ src/main/java/com/example/photomanager/util/SpringContextUtils.java
@@ -0,0 +1,29 @@
+package com.example.photomanager.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 小朝
+ * @date 2020/5/2
+ **/
+@Component
+public class SpringContextUtils implements ApplicationContextAware {
+    public static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        SpringContextUtils.applicationContext = applicationContext;
+    }
+
+    public static Object getBean(String name) {
+        return applicationContext.getBean(name);
+    }
+
+    public static <T> T getBean(String name, Class<T> requiredType) {
+        return applicationContext.getBean(name, requiredType);
+    }
+}
