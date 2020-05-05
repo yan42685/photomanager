cb19a5a76e6a4032274d1904b7161fb24550bde5
alex <419725794@qq.com>
2020-05-05 14:41:31 +0800

    支持CORS跨域请求 修改modifyUserInfo方法

--- src/main/java/com/example/photomanager/service/UserService.java
+++ src/main/java/com/example/photomanager/service/UserService.java
@@ -1,24 +1,23 @@
 package com.example.photomanager.service;
 
+import com.example.photomanager.bean.dto.ModifiableUserInfo;
 import com.example.photomanager.bean.dto.RegistryInfo;
 import com.example.photomanager.bean.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.photomanager.bean.vo.UserInfo;
-import com.example.photomanager.enums.UserInfoEnum;
-
-import javax.servlet.http.HttpSession;
 
 
 public interface UserService extends IService<User> {
     /**
      * 修改用户信息
      */
-    boolean modifyInfo(UserInfoEnum field, String data);
+    boolean modifyInfo(ModifiableUserInfo info);
 
     /**
      * 注册 返回的头像是预设的
      * 如果插入不成功，返回空的UserInfo
      * 插入成功，返回实际UserInfo
+     *
      * @param info 传入的info
      * @return 返回UserInfo
      */
@@ -37,8 +36,8 @@ public interface UserService extends IService<User> {
     boolean checkEmail(String email);
 
     /**
-     * @param username 传入的用户名
-     * @param password 传入的密码
+     * @param username   传入的用户名
+     * @param password   传入的密码
      * @param rememberMe 是否勾选记住密码十天
      * @return 不抛异常，证明登录成功，直接返回true
      */
--- src/test/java/com/example/photomanager/service/impl/AlbumServiceImplTest.java
+++ src/test/java/com/example/photomanager/service/impl/AlbumServiceImplTest.java
@@ -2,14 +2,17 @@ package com.example.photomanager.service.impl;
 
 import com.example.photomanager.bean.vo.AlbumInfo;
 import com.example.photomanager.service.AlbumService;
+import org.apache.shiro.util.ThreadContext;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
  * @author LuckyCurve
@@ -20,6 +23,14 @@ class AlbumServiceImplTest {
 
     @Autowired
     AlbumService albumService;
+
+    @Resource
+    org.apache.shiro.mgt.SecurityManager securityManager;
+
+    @BeforeEach
+    public void setUp() {
+        ThreadContext.bind(securityManager);
+    }
 
     /**
      * 对相册的增删改查进行测试
